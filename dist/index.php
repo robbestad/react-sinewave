7e3faa6851e242063aec3d4e8ddbc0e2bcf29151
Sven Anders Robbestad <robbestad@gmail.com>
2014-09-22 22:13:38 +0200

    single wave

--- dist/index.php
+++ dist/index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<head>
-    <meta http-equiv='Content-type' content='text/html; charset=utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Picture Element Demo</title>
-    <link href="css/style.min.css" type="text/css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
 <?php
+$json = json_decode(file_get_contents(__DIR__."/../package.json"),true);
 require __DIR__.'/php/get_ip.php';
 
 if (@$details->country === "NO") {
@@ -30,10 +13,25 @@ if (@$details->country === "NO") {
     $l_kontakt = "Contact";
     $l_kode = "Code";
 }
-
-$json = json_decode(file_get_contents(__DIR__."/../package.json"),true);
 ?>
+<!DOCTYPE html>
+<head>
+    <meta http-equiv='Content-type' content='text/html; charset=utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="<?php echo $json["name"]; ?> <?php echo $json["version"]; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $json["name"]; ?></title>
+    <link href="css/style.min.css" type="text/css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
+</head>
+
+<body>
 <div class="container">
     <div class="header">
         <ul class="nav nav-pills pull-right">
